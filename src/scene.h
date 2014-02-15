9b1a3474f17de281c2592a19dd61a8e2706cfbc7
jeando <jean-do.favreau@hotmail.fr>
2014-02-15 08:43:18 +0000

    scene

--- src/scene.h
+++ src/scene.h
@@ -23,9 +23,9 @@
 //#include"cloud_segmentation.h"
 #include"types.h"
 
-
 typedef CGAL::Exact_predicates_inexact_constructions_kernel Kernel;
-typedef CGAL::Triangulation_vertex_base_3<Kernel> Vertex_base;
+typedef CGAL::Triangulation_vertex_base_with_info_3<unsigned int, Kernel> Vertex_base;
+//typedef CGAL::Triangulation_vertex_base_3<Kernel> Vertex_base;
 
 typedef CGAL::Triangulation_data_structure_3<Vertex_base> Tds;
 typedef CGAL::Delaunay_triangulation_3<Kernel, Tds> Delaunay;
@@ -39,6 +39,8 @@ public:
 //	ScanLidar sl;
 	Delaunay delaunay;
 	std::vector<Point_d> vpoint;
+	std::vector<Point_d> vcolor_point;
+        std::vector<std::pair<Point_d, unsigned int>> vpoint_indice;
 private:
 
 public:
@@ -62,8 +64,58 @@ public:
     //void loadPLYcertis(const std::string& );
     void compute_delaunay()
     {
-	    delaunay.insert(vpoint.begin(),vpoint.end());
-	    std::cout << vpoint.size() << std::endl;
+	    delaunay.insert(vpoint_indice.begin(),vpoint_indice.end());
+//	    std::cout << vpoint.size() << std::endl;
+	    std::ofstream ofs;
+	    ofs.open("test.off");
+	    ofs << "OFF" << std::endl;
+	    ofs << vpoint_indice.size() << ' ' << delaunay.number_of_finite_facets() << " 0" << std::endl;
+	    for(const auto& a : vpoint_indice)
+		    ofs << a.first.x() << ' ' << a.first.y()
+			    << ' ' << a.first.z() << std::endl;
+	    unsigned int nb_face =0;
+	    for(decltype(delaunay.finite_facets_begin()) facet = delaunay.finite_facets_begin(),
+			    end = delaunay.finite_facets_end();
+			    facet!=end;++facet)
+	    {
+//		std::cerr << facet->second << " ";
+//		it->first->vertex( (it->second+k)%4 )->point(), k=1,2,3//
+//		std::cerr << facet->first->vertex(0)->point().x() << " ";
+//		std::cerr << facet->first->vertex(1)->point().x() << " ";
+//		std::cerr << facet->first->vertex(3)->point().x() << " ";
+//
+//		std::cout << facet->first->vertex(0)->info()<<std::endl;
+//
+
+		    const auto& p1 = facet->first->vertex( (facet->second+1)%4 )->point();
+		    const auto& p2 = facet->first->vertex( (facet->second+2)%4 )->point();
+		    const auto& p3 = facet->first->vertex( (facet->second+3)%4 )->point();
+//			    std::cerr << CGAL::cross_product(p2-p1,p3-p1).squared_length()<<" ";
+		    if(CGAL::cross_product(p2-p1,p3-p1).squared_length()<.0001)
+		    {
+			++nb_face;
+			const int& i1 = facet->first->vertex( (facet->second+1)%4 )->info();
+			const int& i2 = facet->first->vertex( (facet->second+2)%4 )->info();
+			const int& i3 = facet->first->vertex( (facet->second+3)%4 )->info();
+			const Point_d& p1 = vcolor_point[i1];
+			const Point_d& p2 = vcolor_point[i2];
+			const Point_d& p3 = vcolor_point[i3];
+		ofs << 3 << ' '
+			 << i1 << ' '
+			 << i2 << ' '
+			 << i3 << ' '
+			 << 1.0 << ' '
+			 << (p1.z()) << ' '
+			 << (p1.y()) << ' '
+			 << (p1.x()) << ' '
+			 //<< ((p1.z()+p2.z()+p3.z())/3.0) << ' '
+			 //<< ((p1.y()+p2.y()+p3.y())/3.0) << ' '
+			 //<< ((p1.x()+p2.x()+p3.x())/3.0) << ' '
+			 <<std::endl;
+		}
+	    }
+		    std::cerr << nb_face << std::endl;
+	    ofs.close();
 //	for(const auto& hpoint : vpoint)
 //	{
 //		delaunay.insert(hpoint.position);
